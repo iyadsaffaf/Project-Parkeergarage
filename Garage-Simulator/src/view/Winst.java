f95c01280ea549020be4fa7559ea0e0092add87e
Batul Khatib <b.khatib@st.hanze.nl>
2019-01-28 15:52:41 +0100

    winst update

--- Garage-Simulator/src/view/Winst.java
+++ Garage-Simulator/src/view/Winst.java
@@ -1,9 +1,16 @@
 package view;
 
 
+import java.util.Random;
+
+import javafx.animation.AnimationTimer;
+import javafx.collections.FXCollections;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.PieChart;
 import javafx.scene.chart.XYChart;
+
+//Batul khatib
 
 public class Winst extends LineChart <Number,Number>{
 	final static NumberAxis xAxis = new NumberAxis();
@@ -35,7 +42,40 @@ public class Winst extends LineChart <Number,Number>{
         series.getData().add(new XYChart.Data(11, 29));
         series.getData().add(new XYChart.Data(12, 25));
         
+        getData().add(series);
         
+        
+        AnimationTimer d = new AnimationTimer() {
+            private long lastUpdate = 0 ;
+
+			@Override
+			public void handle(long now) {
+				if (now - lastUpdate >= 500_000_000) {
+                   lastUpdate = now ;
+				
+				
+				
+				   Random d = new Random();
+	         	    
+	          
+	            int x = d.nextInt(30);
+	       
+//	            series.getData().add(new XYChart.Data(1, x));
+//	            series.getData().add(new XYChart.Data(2, x));
+//	            series.getData().add(new XYChart.Data(3, 15));
+//	            series.getData().add(new XYChart.Data(4, 24));
+//	            series.getData().add(new XYChart.Data(5, 34));
+//	            series.getData().add(new XYChart.Data(6, 36));
+//	            series.getData().add(new XYChart.Data(7, 22));
+//	            series.getData().add(new XYChart.Data(8, 45));
+//	            series.getData().add(new XYChart.Data(9, 43));
+//	            getData().add(series);
+	            
+	            
+			}}
+		};
+   d.start();
+   
 	}
 	
 
