611a3922487e0a470a5bf64d5e8d00a72f4312c2
strameff <p.jeffreyhamstra@gmail.com>
2019-01-30 15:22:53 +0100

    Commentaar toegevoegd

--- Garage-Simulator/src/controller/Controller.java
+++ Garage-Simulator/src/controller/Controller.java
@@ -164,7 +164,7 @@ public class Controller implements Initializable {
 		Autos = new AantalAutosView(model);
 		paneAutos.getChildren().add(Autos);
 
-		winst = new Winst();
+		winst = new Winst(model);
 		paneWinst.getChildren().add(winst);
 
 		reseveren = new Reserveren();
