c69ac6303fbaf3fc77de65fe85900276611dd514
iyadsaffaf <46675696+iyadsaffaf@users.noreply.github.com>
2019-01-31 00:39:47 +0100

    Resereveren is werkt nu

--- Garage-Simulator/src/view/Reserveren.java
+++ Garage-Simulator/src/view/Reserveren.java
@@ -1,9 +1,7 @@
 package view;
 
-
 import javafx.animation.AnimationTimer;
 import javafx.scene.chart.AreaChart;
-//import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import model.SimulatorModel;
@@ -12,68 +10,69 @@ import model.SimulatorModel;
  * @author Batul Khatib
  * */
 
-public class Reserveren  extends AreaChart <Number,Number> {
-	 final static NumberAxis xAxis = new NumberAxis();
-     final static NumberAxis yAxis = new NumberAxis();
-     private SimulatorModel model;
+public class Reserveren extends AreaChart<Number, Number> {
+
+	private SimulatorModel model;
+	final static NumberAxis xAxis = new NumberAxis(1, 31, 1);
+	final static NumberAxis yAxis = new NumberAxis();
+	private XYChart.Series seriesApril;
+	private int day=0;
+	private int lastTotal=0;
 
 	public Reserveren(SimulatorModel model) {
-		super(xAxis,yAxis);
+		super(xAxis, yAxis);
 		this.model = model;
-	
-        
-       setTitle("Reseveren");
-        xAxis.setLabel("Reseveren");  
-        yAxis.setLabel("Uren");  
-       
-        setAnimated(false);
-        AnimationTimer d = new AnimationTimer() {
-            private long lastUpdate = 0 ;
+
+		seriesApril = new XYChart.Series();
+
+		setTitle("Temperature Monitoring (in Degrees C)");
+		seriesApril.getData().add(new XYChart.Data(0,0));
+
+
+		seriesApril.setName("April");
+
+
+		AnimationTimer d = new AnimationTimer() {
+			private long lastUpdate = 0;
 
 			@Override
 			public void handle(long now) {
 				if (now - lastUpdate >= 500_000_000) {
-                   lastUpdate = now ;
-				
-				
-                   updateGrafiek();
-                   
-				}}
-			};
-			
-		d.start();
-		    
+					lastUpdate = now;
+
+                   update();
+
+				}
 			}
-	         	    
-	          
-	           
-             
-	private void updateGrafiek() {
-		
-                   getData().clear();
-              XYChart.Series series = new XYChart.Series();
-   		        series.setName("Car Reseveren met abonnement");       
-   		        series.getData().add(new XYChart.Data(model.getNumberOfReserverenCar(), model.getHour()));
-   		        
-   		    
-   		   XYChart.Series series1 = new XYChart.Series();
-		        series1.setName("Car Reseveren met een parking pass");    
-		        series1.getData().add(new XYChart.Data(model.getNumberOfParkingPassCar(),model.getHour()));
-		        
-		      /* XYChart.Series series2 = new XYChart.Series();  
-		        series2.setName("Car Reseveren");   
-		        series2.getData().add(new XYChart.Data(model.getNumberOfReserverenCar(),model.getHour()));*/
-		        
-   		    getData().addAll(series, series1 );
-   		  //getData().add(series);
-   		
-		//getData().add(series2);
-   		
-      
-        
-		
-   			   
-					
-	}	
+		};
+
+		d.start();
+
 	}
-			
+
+
+	
+	private synchronized void update() {
+		if (model.getDay() > day) {
+			try {
+				
+				seriesApril.getData().add(new XYChart.Data(model.getDay(), model.getNumberOfReserverenCar()-lastTotal));
+				
+				getData().addAll(seriesApril);
+			day++;
+			lastTotal= model.getNumberOfReserverenCar();
+			}
+			catch(Exception d) {
+				System.out.print("double");
+				
+				
+			}
+		}
+		if (day == 7) {
+			day = 0;
+
+		}
+
+	}
+	
+}
