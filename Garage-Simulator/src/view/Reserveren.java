45dbcc1efcb3f6d8b49f229619c04e1b9af2d076
Batul Khatib <b.khatib@st.hanze.nl>
2019-01-31 12:11:28 +0100

    Commentar update Reserveren, ParkringPassCar en ReseverenCar klassen

--- Garage-Simulator/src/view/Reserveren.java
+++ Garage-Simulator/src/view/Reserveren.java
@@ -8,8 +8,10 @@ import javafx.scene.chart.XYChart;
 import model.SimulatorModel;
 
 /*
+ * Class Reseveren - Laat een AreaChart zien met daarin de data van alle resverende auto's voor de parkeergarage
  * @author Batul Khatib
- * */
+ * @version  5.3
+ * * */
 
 public class Reserveren extends AreaChart<Number, Number> {
 
@@ -21,13 +23,19 @@ public class Reserveren extends AreaChart<Number, Number> {
 
 	private int day = 0;
 	private int lastTotal = 0;
-
+	/**
+     * Constructor for objects of class Reseveren view
+     */
+    /**
+     * Constructor voor objecten van klasse Reseveren view.
+     */
 	public Reserveren(SimulatorModel model) {
 		super(xAxis, yAxis);
 		this.model = model;
 
 		seriesDag = new XYChart.Series();
-
+   // Het algemene gegevens van de grafiek.
+		
 		setTitle("Aantal gereserveerde plaatsen per dag");
 		yAxis.setLabel("Plaats(en)");
 		xAxis.setLabel("Dag");
@@ -37,6 +45,8 @@ public class Reserveren extends AreaChart<Number, Number> {
 		setLegendVisible(false);
 		setMaxHeight(380);
 
+		 // AnimationTimer maakt een nieuwe timer aan zodat we actuele data kunnen weergeven
+		
 		AnimationTimer d = new AnimationTimer() {
 			private long lastUpdate = 0;
 
@@ -54,11 +64,14 @@ public class Reserveren extends AreaChart<Number, Number> {
 		d.start();
 
 	}
+	/*
+	 * Deze methode wordt gebruikt om de AreaChart up te daten met actuele data
+	 */
 
 	private synchronized void update() {
 		if (model.getDay() > day) {
 			try {
-
+				// Het toevoegen van data aan de AreaChart        
 				seriesDag.getData()
 						.add(new XYChart.Data(model.getDay(), model.getNumberOfReserverenCar() - lastTotal));
 
