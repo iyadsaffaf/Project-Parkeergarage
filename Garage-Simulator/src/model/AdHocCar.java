7c339634f7cd4a582e20fc68572b900d57fc91ee
strameff <p.jeffreyhamstra@gmail.com>
2019-01-31 11:52:29 +0100

    AdhocCar commands

--- Garage-Simulator/src/model/AdHocCar.java
+++ Garage-Simulator/src/model/AdHocCar.java
@@ -5,23 +5,34 @@ import java.awt.*;
 import javafx.scene.paint.Color;
 
 public class AdHocCar extends Car {
-	private static final Color COLOR=Color.RED;
-	
-    public AdHocCar() {
-    	Random random = new Random();
-    	int stayMinutes = (int) (15 + random.nextFloat() * 3 * 60);
-        this.setMinutesLeft(stayMinutes);
-        this.setHasToPay(true);
-        this.setStayMinutes(stayMinutes);
-    }
-    
-    public Color getColor(){
-    	return COLOR;
-    }
+	private static final Color COLOR = Color.RED;
 
+	/*
+	 * Constructor voor AdHocCar
+	 * 
+	 * @Version 5.3
+	 */
+	public AdHocCar() {
+		Random random = new Random();
+		int stayMinutes = (int) (15 + random.nextFloat() * 3 * 60);
+		this.setMinutesLeft(stayMinutes);
+		this.setHasToPay(true);
+		this.setStayMinutes(stayMinutes);
+	}
+
+	/*
+	 * @return Color car;
+	 * @see model.Car#getColor()
+	 */
+	public Color getColor() {
+		return COLOR;
+	}
+	/*
+	 * @return String TypeCar
+	 * @see model.Car#getType()
+	 */
 	@Override
 	public String getType() {
-		// TODO Auto-generated method stub
 		return "1";
 	}
 }
