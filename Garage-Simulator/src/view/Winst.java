bcb11bb6816b4a1db6fafd7983e90c9b8b60905d
strameff <p.jeffreyhamstra@gmail.com>
2019-01-31 12:58:26 +0100

    Commentaar toegevoegd

--- Garage-Simulator/src/view/Winst.java
+++ Garage-Simulator/src/view/Winst.java
@@ -7,42 +7,34 @@ import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import model.SimulatorModel;
 
-/*
- *Class Winst - Laat een LineChart zien met daarin de data van alle de omzet van geparkeerde auto's in de garage per uur
- * @author Batul Khatib
- * @version  5.3
- * * */
-
 public class Winst extends LineChart<Number, Number> {
-
+	// defining the axes
 	final static NumberAxis xAxis = new NumberAxis();
 	final static NumberAxis yAxis = new NumberAxis();
 	private int day = 0;
 	private SimulatorModel model;
 	private Series series;
 	private Series oldseries;
+	private int count=0;
 
-	/*
-	 * * Constructor voor objecten van klasse Winst"omzet" view.
-	 */
 	public Winst(SimulatorModel model) {
 		super(xAxis, yAxis);
 		this.model = model;
-		// Het algemene gegevens van de grafiek.
-		xAxis.setLabel("Dag");
+
+		xAxis.setLabel("Uur");
 		yAxis.setLabel("Euro's");
 
+		// defining a series
 		series = new XYChart.Series();
 		series.setName("Omzet");
 
 		series.getData().add(new XYChart.Data(0, 0));
 
-		setTitle("Omzet per dag");
+		setTitle("Omzet per uur");
 		setLegendVisible(false);
 		setMaxHeight(380);
 		setAnimated(false);
-		// AnimationTimer maakt een nieuwe timer aan zodat we actuele data kunnen
-		// weergeven
+
 		AnimationTimer d = new AnimationTimer() {
 			private long lastUpdate = 0;
 
@@ -58,15 +50,12 @@ public class Winst extends LineChart<Number, Number> {
 
 	}
 
-	/*
-	 * Deze methode wordt gebruikt om de LineChart up te daten met actuele data
-	 */
 	private synchronized void update() {
-		if (model.getDay() > day) {
+		if (model.getHour() > day) {
 			try {
-
-				series.getData().add(new XYChart.Data(model.getDay(), model.getProfitPerDay().get(day)));
-				day++;
+                
+				series.getData().add(new XYChart.Data(count, model.getProfitPerDay().get(count)));
+				count++;
 				if (oldseries != series) {
 
 					getData().add(series);
@@ -77,11 +66,15 @@ public class Winst extends LineChart<Number, Number> {
 				System.out.print("double");
 
 			}
-		}
-		if (model.getDay() > 6) {
-			day = 0;
+			
+			if (model.getHour() == 23) {
+				day = 0;
 
+			}else {
+				day++;
+			}
 		}
+		
 
 	}
 }
